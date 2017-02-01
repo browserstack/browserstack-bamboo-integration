9c20c341b0e685d8c79c669477601eec6332b1e9
Pulkit Sharma <avastpulkit@gmail.com>
2017-02-01 15:07:55 +0530

    Stopping BrowserStackLocal binary only if local is set to true

--- src/main/java/com/browserstack/bamboo/ci/action/PostBuildAction.java
+++ src/main/java/com/browserstack/bamboo/ci/action/PostBuildAction.java
@@ -8,28 +8,64 @@ import com.browserstack.bamboo.ci.singletons.BrowserStackLocalSingleton;
 import com.browserstack.bamboo.ci.local.BambooBrowserStackLocal;
 import com.atlassian.spring.container.ContainerManager;
 import org.jetbrains.annotations.NotNull;
+import com.browserstack.bamboo.ci.BStackConfigManager;
+import com.atlassian.bamboo.configuration.AdministrationConfiguration;
+import com.atlassian.bamboo.configuration.AdministrationConfigurationAccessor;
+
+
+/*
+  Post Build Action. Stop the BrowserStack Local Binary if it is running.
+*/
+
+/**
+ * @author Pulkit Sharma
+ */
 
 public class PostBuildAction implements CustomBuildProcessor {
 
     BuildContext buildContext;
 
+    private AdministrationConfigurationAccessor administrationConfigurationAccessor;
+    private BStackConfigManager configManager;
+
     @NotNull
     public BuildContext call() {
+      this.configManager = new BStackConfigManager(administrationConfigurationAccessor.getAdministrationConfiguration(), buildContext.getBuildDefinition().getCustomConfiguration());
+
+      if(configManager.hasCredentials()) {
+        if(configManager.localEnabled()) {
+          stopLocal();
+        }
+      }
+      
+      return buildContext;
+    }
+
+
+    private void stopLocal() {
       BuildLoggerManager buildLoggerManager = (BuildLoggerManager) ContainerManager.getComponent("buildLoggerManager");
       final BuildLogger buildLogger = buildLoggerManager.getLogger(buildContext.getResultKey());
-      
+
       BambooBrowserStackLocal browserStackLocal = BrowserStackLocalSingleton.getBrowserStackLocal("","","");
+
       try {
         browserStackLocal.stop();
         buildLogger.addBuildLogEntry("BrowserStackLocal Binary stopped successfully. LocalIdentifier: " + browserStackLocal.getLocalIdentifier());
       } catch (Exception e) {
         buildLogger.addBuildLogEntry("Exception while stopping the BrowserStackLocal Binary : " + e.toString());
       }
-
-      return buildContext;
     }
+
 
     public void init(@NotNull BuildContext context) {
         this.buildContext = context;
+    }
+
+    public AdministrationConfigurationAccessor getAdministrationConfigurationAccessor() {
+        return administrationConfigurationAccessor;
+    }
+
+    public void setAdministrationConfigurationAccessor(AdministrationConfigurationAccessor administrationConfigurationAccessor) {
+        this.administrationConfigurationAccessor = administrationConfigurationAccessor;
     }
 }
